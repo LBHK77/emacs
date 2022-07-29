a4ab481a30dc218fa9ea606137b18fe212bfb659
LBHK77 <92059068+LBHK77@users.noreply.github.com>
2022-07-29 13:18:08 -0300

    Add files via upload

---
+++ init.el
@@ -0,0 +1,45 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package
+   '(ement :type git :host github :repo "alphapapa/ement.el"))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'ir-black t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f5dd054d9b18c955b3b2bca2f076955e74dcc564f45a093ddf984bdc5b2678d3" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(window-divider ((t (:foreground "dim gray")))))
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+(set-face-foreground 'vertical-border "cyan")
+
+(defalias 'list-buffers 'ibuffer) ; make ibuffer default
